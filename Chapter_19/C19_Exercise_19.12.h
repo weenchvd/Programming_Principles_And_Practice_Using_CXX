ac04e45ae55f4cbcd2b176975a45166034f5889d
weenchvd <d88744@gmail.com>
2020-09-04 18:14:19 +0300

    Exercise 19.12

---
+++ Chapter_19/C19_Exercise_19.12.h
@@ -0,0 +1,26 @@
+﻿/* Exercise 19.12 */
+
+using namespace std;
+
+const char* space_c2 = "  ";
+const char* space_c4 = "    ";
+const char* space_c6 = "      ";
+const char* space_c8 = "        ";
+
+template<class T> class FileHandle {
+public:
+	FileHandle(const string& fileName)
+	{
+		fs.open(fileName);
+		cout << space_c8 << space_c2 << "FileHandle(const string& \"" << fileName << "\")\n";
+	}
+	~FileHandle() { cout << space_c8 << space_c2 << "~FileHandle()\n"; }
+	T& Get() { return fs; }
+
+	operator bool() const { return fs; }
+	bool operator!() const { return !fs; }
+private:
+	T fs;
+};
+
+void ClearInput(istream& is);
